2a16ecbbfb569cd586ef52f30f40e57e230d06f5
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2020-09-08 23:40:54 -0700

    Update Chat to set channel name

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import './Chat.css';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from '../firebase';
+import './Chat.css';
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetaills] = useState(null);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection('rooms')
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetaills(snapshot.data()));
+    }
+  }, [roomId]);
+
   return (
     <div className='chat'>
       <div className='chat-header'>
         <div className='chat-headerLeft'>
           <h4 className='chat-channelName'>
-            <strong>#general</strong>
+            <strong>{roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
